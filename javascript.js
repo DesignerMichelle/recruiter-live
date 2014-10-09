a57c37511decd2d8f3c27da27c38cbf386c94290
RI-8 <ri-8@ri8-MS-7788>
2014-10-09 17:01:37 +0530

    changes

---
+++ javascript.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+			$("#close").click(function(){
+			    alert('hieeeeeee');
+			});
+			$(".img").click(function(){
+			
+			    var photoSrc = $(this).find("img").attr("src");
+			    $(this).append("<div class='big-img-cont'><img src='"+photoSrc+"' /><span class='close' id='close'></span></div>");
+			    $('.overlay').fadeIn();
+			});
+			$(document).mouseup(function (e)
+		{
+		    var container = $(".big-img-cont");
+
+		    if (!container.is(e.target) // if the target of the click isn't the container...
+			   && container.has(e.target).length === 0) // ... nor a descendant of the container
+		    {
+			   //container.hide();
+			   container.remove();
+			   $('.overlay').fadeOut();
+		    }
+		});
+										
+});
